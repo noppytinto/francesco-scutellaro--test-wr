2f8cb8ee7ffbee925ea1152085aea01c99474c53
Francesco Scutellaro <dev.mirror42@gmail.com>
2024-07-02 23:33:07 +0200

    feat: fix image download

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+const isDev = process.env.NODE_ENV === "development";
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: [
@@ -12,7 +15,7 @@ export default defineNuxtConfig({
     },
   },
   image: {
-    provider: "netlify",
+    provider: isDev ? "" : "netlify",
     domains: ["https://images.unsplash.com", "https://randomuser.me"],
   },
   modules: ["@nuxt/image"],
