6ce69356f7df834685a303498c0ddb6c17ef44f2
Francesco Scutellaro <dev.mirror42@gmail.com>
2024-07-02 20:27:28 +0200

    feat: fix faw and add custom colors

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ["~/assets/style/main.scss"],
+  css: [
+    "~/assets/style/main.scss",
+    "@fortawesome/fontawesome-svg-core/styles.css",
+  ],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+
+  modules: ["@nuxt/image"],
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FF4759",
+        secondary: "#4D4D4D",
+        tertiary: "#418089",
+        "my-blue-100": "#F6F8FE",
+        "my-neutral-100": "#DFDFDF",
+        "my-neutral-300": "#999999",
+      },
+    },
   },
   plugins: [],
-}
-
+};
