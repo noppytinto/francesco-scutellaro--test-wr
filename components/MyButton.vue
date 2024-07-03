483e1dae1e07c535dbd8ed6610f46db46c7a1716
Francesco Scutellaro <dev.mirror42@gmail.com>
2024-07-03 16:08:50 +0200

    feat: fix button animation

--- components/MyButton.vue
+++ components/MyButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="rounded-md bg-primary px-4 py-2 font-semibold text-white transition duration-300 ease-in-out hover:shadow-lg focus:outline-none active:scale-95 active:shadow-none"
+    class="rounded-md bg-primary px-4 py-2 font-semibold text-white transition duration-100 ease-in-out hover:shadow-lg focus:outline-none active:scale-95 active:shadow-none"
     @click="$emit('click')"
   >
     <FontAwesomeIcon v-if="icon" :icon class="mr-2" />
